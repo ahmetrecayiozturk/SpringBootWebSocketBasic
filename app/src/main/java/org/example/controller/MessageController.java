0bffbc7f731c5674b61b4b85b2728da9d3d1fe7e
ahmetrecayiozturk <ahmetrecayiozturk@gmail.com>
2025-07-30 00:37:16 +0300

    Some fix
    some fix

--- app/src/main/java/org/example/controller/MessageController.java
+++ app/src/main/java/org/example/controller/MessageController.java
@@ -11,11 +11,27 @@ import org.springframework.web.bind.annotation.RestController;
 @Controller
 public class MessageController {
 
+    // burada messsagemapptin aslında app/** gibi endpointleri belirlememizi sağlar yani ben burada app/sendMessage
+    //endpointine gelen mesajları farklı bir topic'e yönlendirmek istiyorum o farklı topic de aslında üye olduğum channel
+    //yani ben /app/sendMessage endpointine gelen mesajları /topic/sendMessage'a üye
+    //yerlere broadcast ediyorum
     @MessageMapping("/sendMessage")
     @SendTo("/topic/messages")
     public Message sendMessage(Message message) {
         // Burada mesajı işleyebiliriz, örneğin loglayabiliriz
         System.out.println("Received message: " + message);
+        // Mesajı topic'e yayınlıyoruz
+        return message; // Bu mesajı /topic/messages'e yayınlayacak
+    }
+    // burada messsagemapptin aslında app/** gibi endpointleri belirlememizi sağlar yani ben burada app/sendMessageTıDifferentChannel
+    //endpointine gelen mesajları farklı bir topic'e yönlendirmek istiyorum o farklı topic de aslında üye olduğum channel
+    //yani ben /app/sendMessageTıDifferentChannel endpointine gelen mesajları /topic/differentChannel'a üye
+    //yerlere broadcast ediyorum
+    @MessageMapping("/sendMessageTıDifferentChannel")
+    @SendTo("/topic/differentChannel")
+    public Message sendMessageDifferentChannel(Message message) {
+        // Burada mesajı işleyebiliriz, örneğin loglayabiliriz
+        System.out.println("Received message to different channel: " + message);
         // Mesajı topic'e yayınlıyoruz
         return message; // Bu mesajı /topic/messages'e yayınlayacak
     }
